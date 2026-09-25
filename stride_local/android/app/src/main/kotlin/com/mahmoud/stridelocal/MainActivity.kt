87472c00a4bd5e45887c186507192b31cccdbec4
hodasaed28 <hodasaed2428@gmail.com>
2026-09-25 04:56:27 +0300

    Delete stride_local directory

--- stride_local/android/app/src/main/kotlin/com/mahmoud/stridelocal/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.mahmoud.stridelocal
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- stride_local/lib/services/map_service.dart
+++
@@ -1,5 +0,0 @@
-class MapService {
-  static const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-  static const attribution = '© OpenStreetMap contributors';
-  static const userAgent = 'com.mahmoud.stridelocal';
-}
