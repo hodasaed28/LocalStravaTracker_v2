bfdec57a5ae635acf88dad5224d4a838b0a3b2bd
hodasaed28 <hodasaed2428@gmail.com>
2026-09-25 04:57:48 +0300

    Upload lib/services/map_service.dart

---
+++ lib/services/map_service.dart
@@ -0,0 +1,5 @@
+class MapService {
+  static const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+  static const attribution = '© OpenStreetMap contributors';
+  static const userAgent = 'com.mahmoud.stridelocal';
+}
